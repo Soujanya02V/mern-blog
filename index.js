20ea056bbf69b0fcd0b5ca4899797f84ac48f883
Soujanya Maharudra Bailawad <soujanyabailawad@gmail.com>
2025-06-07 08:34:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from "cors"
 import connectToMongo from './config/db.js';
 import router from './routes/blog.js';
 const app = express();
 const port = 9000;
 connectToMongo();
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req,res) => {
     res.send("running");
